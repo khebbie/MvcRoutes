8e1b9b262a0ba939f2c5f47ced39bf344c241c93
Klaus Hebsgaard <khebsgaard@ebay.com>
2011-12-13 14:18:33 +0100

    first real commit

---
+++ MvcRoutes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Routing;
+
+namespace MvcRoutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 1)
+            {
+                Console.WriteLine("Please send the path to your dll as the first argument");
+                return;
+            }
+            CallRegisterRoutes(args);
+
+            foreach (var route in RouteTable.Routes)
+            {
+                var rt = (Route)route;
+                string methodsString = GetMethodsString(rt);
+                
+                Console.WriteLine(rt.Url + " httpMethods: " + methodsString);
+            }
+        }
+
+        private static void CallRegisterRoutes(string[] args)
+        {
+            string ClassName = "MvcApplication";
+            string MethodName = "RegisterRoutes";
+
+            Object[] methodArgs = new[] {RouteTable.Routes};
+
+            Assembly SampleAssembly = Assembly.LoadFrom(args[0]);
+            try
+            {
+                foreach (Type type in SampleAssembly.GetTypes())
+                {
+                    if (type.FullName.EndsWith("." + ClassName))
+                    {
+                        // create an instance of the object
+
+                        object ClassObj = Activator.CreateInstance(type);
+
+                        // Dynamically Invoke the method
+
+                        type.InvokeMember(MethodName,
+                                          BindingFlags.Default | BindingFlags.InvokeMethod,
+                                          null,
+                                          ClassObj,
+                                          methodArgs);
+                    }
+                }
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                Console.WriteLine(ex.LoaderExceptions);
+            }
+        }
+
+        private static string GetMethodsString(Route rt)
+        {
+            string methodsString = "GET";
+            foreach (var constraint in rt.Constraints)
+            {
+                if (constraint.Key == "HttpVerbs")
+                {
+                    var allowedMethods = ((HttpMethodConstraint) constraint.Value).AllowedMethods.ToList();
+                    methodsString = string.Join(", ", allowedMethods);
+                }
+            }
+            return methodsString;
+        }
+    }
+}
